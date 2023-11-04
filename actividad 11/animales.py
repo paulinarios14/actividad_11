a1cce1b82f84c7cfddb118e7e46dfde93a04ccb4
paulinarios14 <146145165+paulinarios14@users.noreply.github.com>
2023-11-03 19:20:41 -0600

    Add files via upload

---
+++ actividad 11/animales.py
@@ -0,0 +1,65 @@
+class Leon:
+    def __init__(self, nombre, edad, peso, color_melena):
+        self.nombre = nombre
+        self.edad = edad
+        self.peso = peso
+        self.color_melena = color_melena
+
+    def rugir(self):
+        print(f"{self.nombre} está rugiendo.")
+
+    def cazar(self):
+        print(f"{self.nombre} está cazando.")
+
+class Elefante:
+    def __init__(self, nombre, edad, peso, tamano_colmillos):
+        self.nombre = nombre
+        self.edad = edad
+        self.peso = peso
+        self.tamano_colmillos = tamano_colmillos
+
+    def trompetear(self):
+        print(f"{self.nombre} está trompeteando.")
+
+    def comer(self):
+        print(f"{self.nombre} está comiendo.")
+
+class Pinguino:
+    def __init__(self, nombre, edad, peso, tipo_plumaje):
+        self.nombre = nombre
+        self.edad = edad
+        self.peso = peso
+        self.tipo_plumaje = tipo_plumaje
+
+    def nadar(self):
+        print(f"{self.nombre} está nadando.")
+
+    def deslizarse(self):
+        print(f"{self.nombre} está deslizándose.")
+
+class Serpiente:
+    def __init__(self, nombre, edad, longitud, tipo_veneno, patron_colores):
+        self.nombre = nombre
+        self.edad = edad
+        self.longitud = longitud
+        self.tipo_veneno = tipo_veneno
+        self.patron_colores = patron_colores
+
+    def reptar(self):
+        print(f"{self.nombre} está reptando.")
+
+    def morder(self):
+        print(f"{self.nombre} está mordiendo.")
+
+class Canguro:
+    def __init__(self, nombre, edad, peso, altura_saltos):
+        self.nombre = nombre
+        self.edad = edad
+        self.peso = peso
+        self.altura_saltos = altura_saltos
+
+    def saltar(self):
+        print(f"{self.nombre} está saltando.")
+
+    def pastar(self):
+        print(f"{self.nombre} está pastando.")
